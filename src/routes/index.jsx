77b7ebf1c434ddaff8d7e78fed98f09b8b3227c7
Maxime <maxsauzeat@gmail.com>
2023-02-07 13:54:40 +0100

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home/Home'
+import LayoutBlank from '../layouts/Blank'
+import LayoutDefault from '../layouts/Default'
+// import Error from '../pages/Error/Error'
+
+export default function Router() {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route element={<LayoutDefault />}>
+               <Route exact path="/" element={<Home />} />
+               <Route path="/about" element={<About />} />
+            </Route>
+
+            <Route element={<LayoutBlank />}>
+               <Route path="*" element={<Error />} />
+			   <Route path="/404" element={<Error />} />
+            </Route>
+         </Routes>
+      </BrowserRouter>
+   )
+}
